6a96403b9d3dc51d328928287dba62ffce4cebe2
cster143 <cster143@gmail.com>
2018-06-20 15:59:48 +0000

    Done.

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,4 +1,4 @@
-STUDENT_NAMES = [
+student_names = [
   "Adele",
   "Beyoncé",
   "Cardi B",
@@ -8,25 +8,79 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  student_names = [
+    "Adele",
+    "Beyoncé",
+    "Cardi B",
+    "Lady Gaga",
+    "Nicki Minaj",
+    "Rihanna"
+  ]
+  return student_names[0]
 end
+first_student_by_index
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  student_names = [
+    "Adele",
+    "Beyoncé",
+    "Cardi B",
+    "Lady Gaga",
+    "Nicki Minaj",
+    "Rihanna"
+  ]
+  return student_names[3]
 end
+fourth_student_by_index
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  student_names = [
+    "Adele",
+    "Beyoncé",
+    "Cardi B",
+    "Lady Gaga",
+    "Nicki Minaj",
+    "Rihanna"
+  ]
+  return student_names[-1]
 end
+last_student_by_index
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+  student_names = [
+    "Adele",
+    "Beyoncé",
+    "Cardi B",
+    "Lady Gaga",
+    "Nicki Minaj",
+    "Rihanna"
+  ]
+  return student_names.first
 end
+first_student_by_method
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+  student_names = [
+    "Adele",
+    "Beyoncé",
+    "Cardi B",
+    "Lady Gaga",
+    "Nicki Minaj",
+    "Rihanna"
+  ]
+  return student_names.last
 end
+last_student_by_method
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+  student_names = [
+    "Adele",
+    "Beyoncé",
+    "Cardi B",
+    "Lady Gaga",
+    "Nicki Minaj",
+    "Rihanna"
+  ]
+  return student_names[0,1,2]
 end
+first_second_and_third_students
